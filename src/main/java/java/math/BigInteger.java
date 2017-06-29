2feb44787c659e82182671600fceb10700f32e20
Erik Schilling <ablu.erikschilling@gmail.com>
2017-06-29 08:41:25 +0200

    Tested implementation of java.math.BigInteger using big-integer

---
+++ src/main/java/java/math/BigInteger.java
@@ -0,0 +1,7 @@
+package java.math;
+
+import static def.big_integer.Globals.bigInt;
+
+public class BigInteger extends def.big_integer.BigInteger {
+    public static def.big_integer.BigInteger ONE = bigInt(1);
+}
